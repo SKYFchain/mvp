402b1b776c945a08f05106345b720b424a66dc4f
valentin.rul <v.rul@qiwi.com>
2018-08-01 12:50:54 +0300

    Working network

---
+++ network.sh
@@ -0,0 +1,431 @@
+export PATH=${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+export VERBOSE=false
+
+# Print the usage message
+function printHelp() {
+  echo "Usage: "
+  echo "  network.sh <mode> [-c <channel name>] [-t <timeout>] [-d <delay>] [-f <docker-compose-file>] [-s <dbtype>] [-l <language>] [-i <imagetag>] [-v]"
+  echo "    <mode> - one of 'up', 'down', 'restart', 'generate' or 'upgrade'"
+  echo "      - 'up' - bring up the network with docker-compose up"
+  echo "      - 'down' - clear the network with docker-compose down"
+  echo "      - 'restart' - restart the network"
+  echo "      - 'generate' - generate required certificates and genesis block"
+  echo "    -c <channel name> - channel name to use (defaults to \"mychannel\")"
+  echo "    -t <timeout> - CLI timeout duration in seconds (defaults to 10)"
+  echo "    -d <delay> - delay duration in seconds (defaults to 3)"
+  echo "    -f <docker-compose-file> - specify which docker-compose file use (defaults to docker-compose-cli.yaml)"
+  echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
+  echo "    -l <language> - the chaincode language: golang (default) or node"
+  echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
+  echo "    -v - verbose mode"
+  echo "  network.sh -h (print this message)"
+  echo
+  echo "Typically, one would first generate the required certificates and "
+  echo "genesis block, then bring up the network. e.g.:"
+  echo
+  echo "	network.sh generate -c mychannel"
+  echo "	network.sh up -c mychannel -s couchdb"
+  echo "        network.sh up -c mychannel -s couchdb -i 1.2.x"
+  echo "	network.sh up -l node"
+  echo "	network.sh down -c mychannel"
+  echo "        network.sh upgrade -c mychannel"
+  echo
+  echo "Taking all defaults:"
+  echo "	network.sh generate"
+  echo "	network.sh up"
+  echo "	network.sh down"
+}
+
+# Ask user for confirmation to proceed
+function askProceed() {
+  read -p "Continue? [Y/n] " ans
+  case "$ans" in
+  y | Y | "")
+    echo "proceeding ..."
+    ;;
+  n | N)
+    echo "exiting..."
+    exit 1
+    ;;
+  *)
+    echo "invalid response"
+    askProceed
+    ;;
+  esac
+}
+
+# Obtain CONTAINER_IDS and remove them
+# TODO Might want to make this optional - could clear other containers
+function clearContainers() {
+  CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer.*.mycc.*/) {print $1}')
+  if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
+    echo "---- No containers available for deletion ----"
+  else
+    docker rm -f $CONTAINER_IDS
+  fi
+}
+
+# Delete any images that were generated as a part of this setup
+# specifically the following images are often left behind:
+# TODO list generated image naming patterns
+function removeUnwantedImages() {
+  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*.mycc.*/) {print $3}')
+  if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
+    echo "---- No images available for deletion ----"
+  else
+    docker rmi -f $DOCKER_IMAGE_IDS
+  fi
+}
+
+# Do some basic sanity checking to make sure that the appropriate versions of fabric
+# binaries/images are available.  In the future, additional checking for the presence
+# of go or other items could be added.
+function checkPrereqs() {
+  # Note, we check configtxlator externally because it does not require a config file, and peer in the
+  # docker image because of FAB-8551 that makes configtxlator return 'development version' in docker
+  LOCAL_VERSION=$(configtxlator version | sed -ne 's/ Version: //p')
+  DOCKER_IMAGE_VERSION=$(docker run --rm hyperledger/fabric-tools:$IMAGETAG peer version | sed -ne 's/ Version: //p' | head -1)
+
+  echo "LOCAL_VERSION=$LOCAL_VERSION"
+  echo "DOCKER_IMAGE_VERSION=$DOCKER_IMAGE_VERSION"
+
+  if [ "$LOCAL_VERSION" != "$DOCKER_IMAGE_VERSION" ]; then
+    echo "=================== WARNING ==================="
+    echo "  Local fabric binaries and docker images are  "
+    echo "  out of  sync. This may cause problems.       "
+    echo "==============================================="
+  fi
+
+  for UNSUPPORTED_VERSION in $BLACKLISTED_VERSIONS; do
+    echo "$LOCAL_VERSION" | grep -q $UNSUPPORTED_VERSION
+    if [ $? -eq 0 ]; then
+      echo "ERROR! Local Fabric binary version of $LOCAL_VERSION does not match this newer version of network and is unsupported. Either move to a later version of Fabric or checkout an earlier version of fabric-samples."
+      exit 1
+    fi
+
+    echo "$DOCKER_IMAGE_VERSION" | grep -q $UNSUPPORTED_VERSION
+    if [ $? -eq 0 ]; then
+      echo "ERROR! Fabric Docker image version of $DOCKER_IMAGE_VERSION does not match this newer version of network and is unsupported. Either move to a later version of Fabric or checkout an earlier version of fabric-samples."
+      exit 1
+    fi
+  done
+}
+
+# Generate the needed certificates, the genesis block and start the network.
+function networkUp() {
+  checkPrereqs
+  # generate artifacts if they don't exist
+  if [ ! -d "crypto-config" ]; then
+    generateCerts
+    generateChannelArtifacts
+  fi
+  if [ "${IF_COUCHDB}" == "couchdb" ]; then
+    IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d 2>&1
+  else
+    IMAGE_TAG=$IMAGETAG docker-compose -f $COMPOSE_FILE up -d 2>&1
+  fi
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Unable to start network"
+    exit 1
+  fi
+  # now run the end to end script
+  docker exec cli scripts/script.sh $CHANNEL_NAME $CLI_DELAY $LANGUAGE $CLI_TIMEOUT $VERBOSE
+  if [ $? -ne 0 ]; then
+    echo "ERROR !!!! Test failed"
+    exit 1
+  fi
+}
+
+# Tear down running network
+function networkDown() {
+  docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH down --volumes --remove-orphans
+
+  # Don't remove the generated artifacts -- note, the ledgers are always removed
+  if [ "$MODE" != "restart" ]; then
+    # Bring down the network, deleting the volumes
+    #Delete any ledger backups
+    docker run -v $PWD:/tmp/backup --rm hyperledger/fabric-tools:$IMAGETAG rm -Rf /tmp/backup/ledgers-backup
+    #Cleanup the chaincode containers
+    clearContainers
+    #Cleanup images
+    removeUnwantedImages
+    # remove orderer block and other channel configuration transactions and certs
+    rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
+  fi
+}
+
+
+# We will use the cryptogen tool to generate the cryptographic material (x509 certs)
+# for our various network entities.  The certificates are based on a standard PKI
+# implementation where validation is achieved by reaching a common trust anchor.
+#
+# Cryptogen consumes a file - ``crypto-config.yaml`` - that contains the network
+# topology and allows us to generate a library of certificates for both the
+# Organizations and the components that belong to those Organizations.  Each
+# Organization is provisioned a unique root certificate (``ca-cert``), that binds
+# specific components (peers and orderers) to that Org.  Transactions and communications
+# within Fabric are signed by an entity's private key (``keystore``), and then verified
+# by means of a public key (``signcerts``).  You will notice a "count" variable within
+# this file.  We use this to specify the number of peers per Organization; in our
+# case it's two peers per Org.  The rest of this template is extremely
+# self-explanatory.
+#
+# After we run the tool, the certs will be parked in a folder titled ``crypto-config``.
+
+# Generates Org certs using cryptogen tool
+function generateCerts() {
+  which cryptogen
+  if [ "$?" -ne 0 ]; then
+    echo "cryptogen tool not found. exiting"
+    exit 1
+  fi
+  echo
+  echo "##########################################################"
+  echo "##### Generate certificates using cryptogen tool #########"
+  echo "##########################################################"
+
+  if [ -d "crypto-config" ]; then
+    rm -Rf crypto-config
+  fi
+  set -x
+  cryptogen generate --config=./crypto-config.yaml
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate certificates..."
+    exit 1
+  fi
+  echo
+}
+
+# The `configtxgen tool is used to create four artifacts: orderer **bootstrap
+# block**, fabric **channel configuration transaction**, and two **anchor
+# peer transactions** - one for each Peer Org.
+#
+# The orderer block is the genesis block for the ordering service, and the
+# channel transaction file is broadcast to the orderer at channel creation
+# time.  The anchor peer transactions, as the name might suggest, specify each
+# Org's anchor peer on this channel.
+#
+# Configtxgen consumes a file - ``configtx.yaml`` - that contains the definitions
+# for the sample network. There are three members - one Orderer Org 
+# and two Peer Orgs each managing and maintaining two peer nodes.
+# This file also specifies a consortium - ``SampleConsortium`` - consisting of our
+# two Peer Orgs.  Pay specific attention to the "Profiles" section at the top of
+# this file.  You will notice that we have two unique headers. One for the orderer genesis
+# block - and one for our channel.
+# These headers are important, as we will pass them in as arguments when we create
+# our artifacts.  This file also contains two additional specifications that are worth
+# noting.  Firstly, we specify the anchor peers for each Peer Org.
+# Secondly, we point to
+# the location of the MSP directory for each member, in turn allowing us to store the
+# root certificates for each Org in the orderer genesis block.  This is a critical
+# concept. Now any network entity communicating with the ordering service can have
+# its digital signature verified.
+#
+# This function will generate the crypto material and our four configuration
+# artifacts, and subsequently output these files into the ``channel-artifacts``
+# folder.
+#
+# If you receive the following warning, it can be safely ignored:
+#
+# [bccsp] GetDefault -> WARN 001 Before using BCCSP, please call InitFactories(). Falling back to bootBCCSP.
+#
+# You can ignore the logs regarding intermediate certs, we are not using them in
+# this crypto implementation.
+
+# Generate orderer genesis block, channel configuration transaction and
+# anchor peer update transactions
+function generateChannelArtifacts() {
+  which configtxgen
+  if [ "$?" -ne 0 ]; then
+    echo "configtxgen tool not found. exiting"
+    exit 1
+  fi
+
+  echo "##########################################################"
+  echo "#########  Generating Orderer Genesis block ##############"
+  echo "##########################################################"
+  # Note: For some unknown reason (at least for now) the block file can't be
+  # named orderer.genesis.block or the orderer will fail to launch!
+  set -x
+  configtxgen -profile SkyfOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate orderer genesis block..."
+    exit 1
+  fi
+  echo
+  echo "#################################################################"
+  echo "### Generating channel configuration transaction 'channel.tx' ###"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile SkyfChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate channel configuration transaction..."
+    exit 1
+  fi
+
+  echo
+  echo "#################################################################"
+  echo "#######    Generating anchor peer update for Customer1MSP   ##########"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile SkyfChannel -outputAnchorPeersUpdate ./channel-artifacts/Customer1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Customer1MSP
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate anchor peer update for Custome1MSP..."
+    exit 1
+  fi
+
+  echo
+  echo "#################################################################"
+  echo "#######    Generating anchor peer update for Operator1MSP   ##########"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile SkyfChannel -outputAnchorPeersUpdate \
+    ./channel-artifacts/Operator1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Operator1MSP
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate anchor peer update for Operator1MSP..."
+    exit 1
+  fi
+
+  echo
+  echo "#################################################################"
+  echo "#######    Generating anchor peer update for Admin1MSP   ##########"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile SkyfChannel -outputAnchorPeersUpdate \
+    ./channel-artifacts/Admin1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Admin1MSP
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate anchor peer update for Admin1MSP..."
+    exit 1
+  fi
+
+  echo
+  echo "#################################################################"
+  echo "#######    Generating anchor peer update for Monitor1MSP   ##########"
+  echo "#################################################################"
+  set -x
+  configtxgen -profile SkyfChannel -outputAnchorPeersUpdate \
+    ./channel-artifacts/Monitor1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Monitor1MSP
+  res=$?
+  set +x
+  if [ $res -ne 0 ]; then
+    echo "Failed to generate anchor peer update for Monitor1MSP..."
+    exit 1
+  fi
+
+  echo
+}
+
+# Obtain the OS and Architecture string that will be used to select the correct
+# native binaries for your platform, e.g., darwin-amd64 or linux-amd64
+OS_ARCH=$(echo "$(uname -s | tr '[:upper:]' '[:lower:]' | sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')" | awk '{print tolower($0)}')
+# timeout duration - the duration the CLI should wait for a response from
+# another container before giving up
+CLI_TIMEOUT=10
+# default for delay between commands
+CLI_DELAY=3
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mychannel"
+# use this as the default docker-compose yaml definition
+COMPOSE_FILE=docker-compose-cli.yaml
+#
+COMPOSE_FILE_COUCH=docker-compose-couch.yaml
+#
+# use golang as the default language for chaincode
+LANGUAGE=golang
+# default image tag
+IMAGETAG="latest"
+# Parse commandline args
+if [ "$1" = "-m" ]; then # supports old usage, muscle memory is powerful!
+  shift
+fi
+MODE=$1
+shift
+# Determine whether starting, stopping, restarting, generating or upgrading
+if [ "$MODE" == "up" ]; then
+  EXPMODE="Starting"
+elif [ "$MODE" == "down" ]; then
+  EXPMODE="Stopping"
+elif [ "$MODE" == "restart" ]; then
+  EXPMODE="Restarting"
+elif [ "$MODE" == "generate" ]; then
+  EXPMODE="Generating certs and genesis block"
+elif [ "$MODE" == "upgrade" ]; then
+  EXPMODE="Upgrading the network"
+else
+  printHelp
+  exit 1
+fi
+
+while getopts "h?c:t:d:f:s:l:i:v" opt; do
+  case "$opt" in
+  h | \?)
+    printHelp
+    exit 0
+    ;;
+  c)
+    CHANNEL_NAME=$OPTARG
+    ;;
+  t)
+    CLI_TIMEOUT=$OPTARG
+    ;;
+  d)
+    CLI_DELAY=$OPTARG
+    ;;
+  f)
+    COMPOSE_FILE=$OPTARG
+    ;;
+  s)
+    IF_COUCHDB=$OPTARG
+    ;;
+  l)
+    LANGUAGE=$OPTARG
+    ;;
+  i)
+    IMAGETAG=$(go env GOARCH)"-"$OPTARG
+    ;;
+  v)
+    VERBOSE=true
+    ;;
+  esac
+done
+
+
+# Announce what was requested
+
+if [ "${IF_COUCHDB}" == "couchdb" ]; then
+  echo
+  echo "${EXPMODE} for channel '${CHANNEL_NAME}' with CLI timeout of '${CLI_TIMEOUT}' seconds and CLI delay of '${CLI_DELAY}' seconds and using database '${IF_COUCHDB}'"
+else
+  echo "${EXPMODE} for channel '${CHANNEL_NAME}' with CLI timeout of '${CLI_TIMEOUT}' seconds and CLI delay of '${CLI_DELAY}' seconds"
+fi
+# ask for confirmation to proceed
+askProceed
+
+#Create the network using docker compose
+if [ "${MODE}" == "up" ]; then
+  networkUp
+elif [ "${MODE}" == "down" ]; then ## Clear the network
+  networkDown
+elif [ "${MODE}" == "generate" ]; then ## Generate Artifacts
+  generateCerts
+  generateChannelArtifacts
+elif [ "${MODE}" == "restart" ]; then ## Restart the network
+  networkDown
+  networkUp
+else
+  printHelp
+  exit 1
+fi
---
+++ scripts/script.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+echo
+echo " ____    _____      _      ____    _____ "
+echo "/ ___|  |_   _|    / \    |  _ \  |_   _|"
+echo "\___ \    | |     / _ \   | |_) |   | |  "
+echo " ___) |   | |    / ___ \  |  _ <    | |  "
+echo "|____/    |_|   /_/   \_\ |_| \_\   |_|  "
+echo
+echo "End-to-end test"
+echo
+CHANNEL_NAME="$1"
+DELAY="$2"
+LANGUAGE="$3"
+TIMEOUT="$4"
+VERBOSE="$5"
+: ${CHANNEL_NAME:="mychannel"}
+: ${DELAY:="3"}
+: ${LANGUAGE:="golang"}
+: ${TIMEOUT:="10"}
+: ${VERBOSE:="false"}
+LANGUAGE=`echo "$LANGUAGE" | tr [:upper:] [:lower:]`
+COUNTER=1
+MAX_RETRY=5
+
+CC_SRC_PATH="github.com/chaincode/chaincode_example02/go/"
+if [ "$LANGUAGE" = "node" ]; then
+	CC_SRC_PATH="/opt/gopath/src/github.com/chaincode/chaincode_example02/node/"
+fi
+
+echo "Channel name : "$CHANNEL_NAME
+
+# import utils
+. scripts/utils.sh
+
+createChannel() {
+	setGlobals 0 "Customer1"
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+                set -x
+		peer channel create -o orderer.skyfchain.io:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx >&log.txt
+		res=$?
+                set +x
+	else
+				set -x
+		peer channel create -o orderer.skyfchain.io:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+		res=$?
+				set +x
+	fi
+	cat log.txt
+	verifyResult $res "Channel creation failed"
+	echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+	echo
+}
+
+joinChannel () {
+	declare -a arr=("Customer1" "Operator1" "Admin1" "Monitor1")
+	for org in "${arr[@]}"; do
+	    for peer in 0 1; do
+		joinChannelWithRetry $peer $org
+		echo "===================== peer${peer}.${org} joined channel '$CHANNEL_NAME' ===================== "
+		sleep $DELAY
+		echo
+	    done
+	done
+}
+
+## Create channel
+echo "Creating channel..."
+createChannel
+
+## Join all the peers to the channel
+echo "Having all peers join the channel..."
+joinChannel
+
+## Set the anchor peers for each org in the channel
+echo "Updating anchor peers for customer1..."
+updateAnchorPeers 0 "Customer1"
+echo "Updating anchor peers for operator1..."
+updateAnchorPeers 0 "Operator1"
+echo "Updating anchor peers for admin1..."
+updateAnchorPeers 0 "Admin1"
+echo "Updating anchor peers for monitor1..."
+updateAnchorPeers 0 "Monitor1"
+
+
+## Install chaincode on peer0 of each org
+echo "Installing chaincode on peer0.customer1..."
+installChaincode 0 "Customer1"
+echo "Install chaincode on peer0.operator1..."
+installChaincode 0 "Operator1"
+echo "Installing chaincode on peer0.admin1..."
+installChaincode 0 "Admin1"
+echo "Install chaincode on peer0.monitor1..."
+installChaincode 0 "Monitor1"
+
+
+# Instantiate chaincode on peer0.operator1
+echo "Instantiating chaincode on peer0.operator1..."
+instantiateChaincode 0 "Operator1"
+
+# Query chaincode on peer0.customer1
+echo "Querying chaincode on peer0.customer1..."
+chaincodeQuery 0 "Customer1" 100
+
+# Invoke chaincode on peer0.customer1 and peer0.operator1
+echo "Sending invoke transaction on peer0.customer1 peer0.operator1..."
+chaincodeInvoke 0 "Customer1" 0 "Operator1" 0 "Admin1" 0 "Monitor1"
+
+# Install chaincode on peer1.operator1
+echo "Installing chaincode on peer1.operator1..."
+installChaincode 1 "Operator1"
+
+# Query on chaincode on peer1.operator1, check if the result is 90
+echo "Querying chaincode on peer1.operator1..."
+chaincodeQuery 1 "Operator1" 90
+
+echo
+echo "========= All GOOD, Test execution completed =========== "
+echo
+
+echo
+echo " _____   _   _   ____   "
+echo "| ____| | \ | | |  _ \  "
+echo "|  _|   |  \| | | | | | "
+echo "| |___  | |\  | | |_| | "
+echo "|_____| |_| \_| |____/  "
+echo
+
+exit 0
